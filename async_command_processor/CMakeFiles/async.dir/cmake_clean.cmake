6c5e6ca1e5be1fcbdc7a9d7590a57edda905d9fc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-05 22:41:01 +0000

    Deploy Alkud/OTUS_HOMEWORK_11 to github.com/Alkud/OTUS_HOMEWORK_11.git:gh-pages

--- async_command_processor/CMakeFiles/async.dir/cmake_clean.cmake
+++ async_command_processor/CMakeFiles/async.dir/cmake_clean.cmake
@@ -4,7 +4,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/async.dir/input_reader.cpp.o"
   "CMakeFiles/async.dir/publisher_mt.cpp.o"
   "libasync.pdb"
-  "libasync.so.0.0.61"
+  "libasync.so.0.0.62"
   "libasync.so"
 )
 
